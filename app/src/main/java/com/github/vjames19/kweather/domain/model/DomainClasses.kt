7a800051e1e774354b3bfb560be18734e11aa40d
Victor J. Reventos <victor.reventos1@gmail.com>
2015-10-25 00:02:11 -0500

    Chapter 11

--- app/src/main/java/com/github/vjames19/kweather/domain/model/DomainClasses.kt
+++ app/src/main/java/com/github/vjames19/kweather/domain/model/DomainClasses.kt
@@ -1,4 +1,7 @@
 package com.github.vjames19.kweather.domain.model
 
-data internal class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data internal class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+    public fun get(position: Int): Forecast = dailyForecast[position]
+    public fun size(): Int = dailyForecast.size
+}
 data internal class Forecast(val date: String, val description: String, val high: Int, val low: Int)
